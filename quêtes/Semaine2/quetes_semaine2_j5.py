c0845affd2425580974e2791638366791579f0d5
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2023-11-17 20:23:52 +0100

    apiFlake validation

--- quêtes/Semaine2/quetes_semaine2_j5.py
+++ quêtes/Semaine2/quetes_semaine2_j5.py
@@ -2,11 +2,15 @@
 
 """
 Help for statisticians!
-Your fellow statisticians need you. They want to learn how to program in Python and have tons of calculations to do.
+Your fellow statisticians need you. They want to learn
+how to program in Python and have tons of calculations to do.
 
 Very kindly, you agree to help them. Complete the missions below.
 
-For all functions below, please add an extra cell where you actually call the function on at least two different inputs and displays the returned value (if the function doesn't already display stuff).
+For all functions below, please add an extra cell where you
+actually call the function on at least two different inputs
+and displays the returned value (if the function doesn't
+already display stuff).
 """
 
 # Mission 9: Create a function that takes a day as a parameter and returns the next day (i.e. for Sunday it would return Monday).
@@ -14,28 +18,40 @@ For all functions below, please add an extra cell where you actually call the fu
 
 def get_next_day(your_day):
     list_days=["monday","thuesday","wednesday","thursday","friday","saturday","sunday"]
-    i=0
-    for day in list_days:
-        if day== your_day:
-            current_day_index = days_of_week.index(day)
-        next_day_index = (current_day_index + 1) % 7
-            nextday=list_days[i]
-    return nextday
+    if your_day.lower() in list_days:
+        nextday = list_days.index(your_day.lower())
+        print(nextday)
+        # nextday = (nextday + 1) % 7
+        nextday = [
+            (nextday + 1)
+            if not (nextday + 1)==7
+            else 0
+            for i in range(6)
+              ]
+        print(nextday)
+        list_days=list_days[nextday]
+    else:
+        return
+    # print("the next day: ", list_days)
+    return list_days
+
+
 
 
 # test de la fonction
-day="Sunday"
-get_next_day(day)
 
 
-def next_day(day):
-    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    try:
-        current_day_index = days_of_week.index(day)
-        next_day_index = (current_day_index + 1) % 7
-        return days_of_week[next_day_index]
-    except ValueError:
-        return "Invalid day. Please enter a valid day of the week."
+your_day="sunday"
+print("ma variable: ", get_next_day(your_day))
 
-# Test the function
-print(next_day('Sunday'))  # Output: Monday
+# def next_day(day):
+#     days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+#     try:
+#         current_day_index = days_of_week.index(day)
+#         next_day_index = (current_day_index + 1) % 7
+#         return days_of_week[next_day_index]
+#     except ValueError:
+#         return "Invalid day. Please enter a valid day of the week."
+
+# # Test the function
+# print(next_day('Sunday'))  # Output: Monday
