8b7072c7d6d547e7c1e28e417981fb1a9efe7811
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2023-11-18 20:14:57 +0100

    creation prog personnalité_francaise.py

--- quêtes/Semaine2/personnalites_francaises.py
+++ quêtes/Semaine2/personnalites_francaises.py
@@ -7,26 +7,65 @@ Le programme  demande à l'utlisateur s'il veut modifer le fichier,
 """
 
 # Lire le fichier JSON
-with open('quêtes\data\json\liste_personnalite_francaise.json', 'r') as f:
+with open(r"quêtes\data\json\liste_personnalite_francaise.json", 'r',encoding='utf-8') as f:
     data = json.load(f)
-
-# Choisir une personnalité au hasard
-personnalite = random.choice(list(data.keys()))
+# print("data:", data)
+# Choisir une personnalité au hasard avec toutes ses sous categories
+personnalite = [p[key] for categorie in data.values() for p in categorie for key in p if key.startswith('Personnalités')]
 print(f"Personnalité choisie : {personnalite}")
-print(json.dumps(data[personnalite], indent=4, ensure_ascii=False))
+personnalite = random.choice(personnalite)
+print(f"Personnalité choisie : {personnalite}")
+# print(data[personnalite])
+
+# # Créer une liste de tous les noms
+# noms = [personnalite['nom'] for personnalite in data.values()]
+# noms = list(data.keys())
+#  parcourt chaque toutes les sous-categories du champ "Nom" et les mets dans la liste "noms"
+noms = [p['Nom'] for categorie in data.values() for p in categorie]
+print("noms:",noms)
+# Choisir un nom au hasard
+nom = random.choice(noms)
+print("nom:",nom)
+
+
+if nom is not None:
+    print(f"Personnalité choisie : {nom}")
+    # fait une liste de toutes les sous-categories du champ "Nom"
+    #  s'il est égale = nom
+    categorie_nom = [p for categorie in data.values() for p in categorie if p['Nom'] == nom]
+    # categories = [[v for k, v in p.items()] for categorie in data.values() for p in categorie if p['Nom'] == nom]
+    print("categorie_nom",categorie_nom)
+    # Imprimer chaque champ et sa valeur
+    for champ, valeur in nom.items():
+        print(f"{champ}: {valeur}")
+else:
+    print("Aucune personnalité trouvée avec ce nom.")
 
 # Demander à l'utilisateur s'il souhaite modifier le fichier
-modifier = input("Voulez-vous modifier le fichier ? (oui/non) ")
+# modifier = input("Voulez-vous modifier le fichier ? (oui/non) ")
 
-if modifier.lower() == 'oui':
-    # Demander à l'utilisateur quel champ il souhaite modifier
-    champ = input("Quel champ souhaitez-vous modifier ? ")
-    nouvelle_valeur = input("Quelle est la nouvelle valeur ? ")
+# if modifier.lower() in ('oui','o','yes','y'):
+#     # Demander à l'utilisateur quel champ il souhaite modifier
+#     champ = input("Quel champ souhaitez-vous modifier ? ")
+#     nouvelle_valeur = input("Quelle est la nouvelle valeur ? ")
 
-    # Modifier le champ
-    data[personnalite][champ] = nouvelle_valeur
+#     # Modifier le champ
+#     data[personnalite][champ] = nouvelle_valeur
+remplacer = input("Voulez-vous remplacer une entrée entière ? (oui/non) ")
+if remplacer.lower() in ('oui', 'o', 'yes', 'y'):
+    # Demander à l'utilisateur quelle entrée remplacer
+    entree = input("Quelle entrée souhaitez-vous remplacer ? ")
+    if entree in data.keys():
+        # Demander les nouvelles données
+        nouvelle_entree = input("Entrez les nouvelles données (au format JSON) : ")
+        nouvelle_entree = json.loads(nouvelle_entree)
+
+        # Remplacer l'entrée
+        data[entree] = nouvelle_entree
+    else:
+        print("Entrée non reconnue.")
 
     # Enregistrer le fichier modifié avec un nouveau nom
     date_du_jour = datetime.now().strftime("%d%b%Y")
-    with open(f'personnalites_francaises_modified_{date_du_jour}.json', 'w') as f:
+    with open(f'personnalites_francaises_modified_{date_du_jour}.json', 'w',encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
