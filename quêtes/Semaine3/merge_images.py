a713c5ff08b9066814b0abf725fcc3c4d88c3182
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2023-11-21 07:45:18 +0100

    Ajout Base wizard

--- quêtes/Semaine3/merge_images.py
+++ quêtes/Semaine3/merge_images.py
@@ -1,21 +1,28 @@
 from PIL import Image
 
 # Ouvrir les images
-img1 = Image.open('quêtes\images\screenshot1.png')
-img2 = Image.open('quêtes\images\screenshot2.png')
+img1 = Image.open('quêtes\images\screenshot3.png')
+img2 = Image.open('quêtes\images\screenshot4.png')
+img3 = Image.open('quêtes\images\screenshot5.png')
+img4 = Image.open('quêtes\images\screenshot6.png')
 # ...
-WIDTH=1280
-HEIGHT=800
+WIDTH = 1280
+HEIGHT = 800
 # Assurez-vous que toutes les images ont la même taille
 img1 = img1.resize((WIDTH, HEIGHT))
 img2 = img2.resize((WIDTH, HEIGHT))
+img3 = img3.resize((WIDTH, HEIGHT))
+img4 = img4.resize((WIDTH, HEIGHT))
 # ...
-nbre_image=2
+nbre_image = 4
 # Fusionner les images
-merged = Image.new('RGBA', (WIDTH, HEIGHT * nbre_image))  # Remplacer N par le nombre d'images
+# Remplacer N par le nombre d'images
+merged = Image.new('RGBA', (WIDTH, HEIGHT * nbre_image))
 merged.paste(img1, (0, 0))
 merged.paste(img2, (0, HEIGHT))
+merged.paste(img3, (0, HEIGHT*2))
+merged.paste(img4, (0, HEIGHT*3))
 # ...
 
 # Enregistrer l'image fusionnée
-merged.save('quêtes\images\question1.png')
+merged.save('quêtes\images\Recuperer_des_informations.png')
