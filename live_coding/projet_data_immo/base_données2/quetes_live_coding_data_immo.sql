a97bc06da894a323b48a63c7750c5bc5704f5089
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2023-11-23 14:41:38 +0100

    warning notebook excel polynomizle

--- live_coding/projet_data_immo/base_données2/quetes_live_coding_data_immo.sql
+++ live_coding/projet_data_immo/base_données2/quetes_live_coding_data_immo.sql
@@ -80,18 +80,83 @@ limit 10
 
 -- --------------------
 -- 5. Prix moyen du mètre carré d’une maison en Île-de-France.
+select substr(localisation.code_postal,1 ,2) as "départemnetcode", --on extrait les 2 premiers caractéres du code postal
+count(vente.id) as "nombre de vente",
+round(avg(valeur_vente/surface_carrez),2) as "prix au m²"
+from bien
+inner join vente
+on bien.id=vente.id
 
+inner join localisation
+on bien.id=localisation.id
+where type_local="Maison"
+and départemnetcode in ("75","77","78","91","92","93","94")
+and surface_carrez >0
+and surface_reelle >0
+group by "départemnetcode"
+ORDER by "prix au m²" DESC
+
+;
 
 
 -- --------------------
 -- 6. Liste des 10 appartements les plus chers avec la région et le nombre
 -- de mètres carrés.
+select substr(localisation.code_postal,1 ,2) as "départemnetcode", --on extrait les 2 premiers caractéres du code postal
+count(vente.id) as "nombre de vente",
+round(avg(valeur_vente/surface_carrez),2) as "prix au m²",
+round(avg(surface_carrez),0) as "surface_réelle",
+round(avg(surface_reelle),0)
+from bien
+inner join vente
+on bien.id=vente.id
+
+inner join localisation
+on bien.id=localisation.id
+where date_vente< "2020-07-01 00:00:00"
+and départemnetcode in ("75","77","78","91","92","93","94")
+and surface_carrez >0
+and surface_reelle >0
+
+group by "départemnetcode"
+ORDER by "prix au m²" DESC
+
+;
 
 
 
 -- --------------------
 -- 7. Taux d’évolution du nombre de ventes entre le premier et le second
 -- trimestre de 2020.
+-- Requête pour le premier trimestre
+WITH
+Q1 AS (
+    SELECT COUNT(vente.id) AS "nombre de vente"
+    FROM bien
+    INNER JOIN vente ON bien.id=vente.id
+    INNER JOIN localisation ON bien.id=localisation.id
+    WHERE date_vente BETWEEN "2020-01-01 00:00:00" AND "2020-03-31 23:59:59"
+    AND surface_carrez >0
+    AND surface_reelle >0
+),
+Q2 AS (
+    SELECT COUNT(vente.id) AS "nombre de vente"
+    FROM bien
+    INNER JOIN vente ON bien.id=vente.id
+    INNER JOIN localisation ON bien.id=localisation.id
+    WHERE date_vente BETWEEN "2020-04-01 00:00:00" AND "2020-06-30 23:59:59"
+    AND surface_carrez >0
+    AND surface_reelle >0
+)
+--calcul du taux d'évolution en % arrondi à 2 aprés la virgule
+SELECT round(((Q2."nombre de vente" - Q1."nombre de vente") * 100.0 / Q1."nombre de vente"),2) || ' %' as "taux d'évolution"
+FROM Q1, Q2;
+
+
+-- 2éme solution avec un lag()
+
+
+
 
 
 -- --------------------
