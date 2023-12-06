c6f745495b961f509409f9b6448e39eb28c66376
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2023-12-06 11:30:40 +0100

    2.1 pandas avec seaborn

--- tests_check_point/sources/script_creation_BDD_3w/Script-base3W.sql
+++ tests_check_point/sources/script_creation_BDD_3w/Script-base3W.sql
@@ -1,13 +1,42 @@
--- Ton code SQL ici :
-use w3schools;
-SELECT c.CategoryName, 
-ROUND(SUM(p.Price*od.Quantity)) as Chiffre_d_affaire, 
-ROUND(SUM(p.Price*od.Quantity)/SUM(od.Quantity),2) as Prix_moyen
+-- crée une requête SQL qui fournit les noms de chaque catégorie, le chiffre d'affaire
+-- associé à cette catégorie, et le prix moyen pondéré des produits vendus pour cette catégorie. 
+-- Le chiffre d'affaire sera arrondi à l'unité la plus proche, et le prix moyen comportera 2 décimales.
+-- Ce tableau devra être trié pour avoir les catégories avec le chiffre d'affaire le plus élevé en premier.
+
+USE w3schools;
+with PrixMoyen as(
+select 
+ROUND((SUM(p.Price*od.Quantity)/SUM(od.Quantity)),2) as "Prix moyen"
+from categories c 
+join products p using(id) 
+
+);
+-- autre solution
+SELECT c.CategoryName as "Catégories", 
+ROUND(SUM(p.Price*od.Quantity)) as "Chiffre d'affaire", 
+ROUND((SUM(p.Price*od.Quantity)/SUM(od.Quantity)),2) as "Prix moyen"
+FROM Categories c
+JOIN Products p using(id) 
+JOIN Order_Details od using(id) 
+GROUP BY c.CategoryID
+ORDER BY "Chiffre d'affaire" DESC;
+
+/* select CONCAT(firstname, ' ', lastname) AS fullname, role
+from player p 
+inner join wizard w  using(id) 
+where enrollment_date between '1995-01-01' and '1998-01-01'
+and role='chaser'
+;
+*/
+-- autre solution
+SELECT c.CategoryName as "Catégories", 
+ROUND(SUM(p.Price*od.Quantity)) as "Chiffre d'affaire", 
+ROUND((SUM(p.Price*od.Quantity)/SUM(od.Quantity)),2) as "Prix moyen"
 FROM Categories c
 JOIN Products p 
 ON c.CategoryID=p.CategoryID
 JOIN Order_Details od 
 ON p.ProductID=od.ProductID
 GROUP BY c.CategoryID
-ORDER BY Chiffre_d_affaire DESC;
+ORDER BY "Chiffre d'affaire" DESC;
         
