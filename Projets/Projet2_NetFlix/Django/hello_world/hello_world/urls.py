37718c2a24d81ae7e2c30e36fe23a09e0e3f05d3
Bigmoletos <32773811+bigmoletos@users.noreply.github.com>
2024-01-07 22:03:31 +0100

    ajout prjet hello world, et formulaire dans recommendations

--- Projets/Projet2_NetFlix/Django/hello_world/hello_world/urls.py
+++ Projets/Projet2_NetFlix/Django/hello_world/hello_world/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projet_recommendation_films project.
+URL configuration for hello_world project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,15 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from django.contrib import admin
-from my_application.views import accueil, kpi, recommendation, annexe, actor_view
+from django.urls import path
+from home.views import home
 
 urlpatterns = [
-    path('', accueil, name='accueil'),
-    path('kpi/', kpi, name='kpi'),
-    path('recommendation/', recommendation, name='recommendation'),
-    path('annexe/', annexe, name='annexe'),
-    path('actor_view/', actor_view, name='actor_view'),
     path('admin/', admin.site.urls),
+    path('',home, name='home'),
 ]
--- Projets/Projet2_NetFlix/Django/projet_recommendation_films/manage.py
+++ Projets/Projet2_NetFlix/Django/projet_recommendation_films/manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projet_recommendation_films.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'projet_recommendation_films.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,3 +22,17 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello_world.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
